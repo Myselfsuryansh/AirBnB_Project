26a7d43b57a55ec9cd4c464c7f92e0f290b2eb31
Myselfsuryansh <suryanshsinhabth11@gmail.com>
2025-12-06 22:59:03 +0530

    Build the Entity for the airbnb project with enums

---
+++ src/main/java/com/codingshuttle/projects/airBnbApp/entity/HotelContactInfo.java
@@ -0,0 +1,17 @@
+package com.codingshuttle.projects.airBnbApp.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Data
+@Embeddable
+public class HotelContactInfo {
+
+    private String address;
+
+    private String phoneNumber;
+
+    private String email;
+    
+    private String location;
+}
---
+++ src/main/java/com/codingshuttle/projects/airBnbApp/enums/BookingStatus.java
@@ -0,0 +1,9 @@
+package com.codingshuttle.projects.airBnbApp.enums;
+
+public enum BookingStatus {
+
+    RESERVED,
+    PENDING,
+    CONFIRMED,
+    CANCELLED
+}
